28ba2458c1312bdb9b269cd4d8064f732ac67347
Bogdana94 <bogdana_boboiciov@yahoo.com>
2016-10-19 23:57:44 +0300

    Tema1

---
+++ Bogdana/PSSC/Models/Decanat.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class Decanat
+    {
+        List<Disciplina> listaDiscipline = new List<Disciplina>();
+        List<Student> listaStudenti = new List<Student>();
+        List<Profesor> listaProfesori = new List<Profesor>();
+
+
+        public void adauga_disciplina(Disciplina d)
+        {
+
+        }
+        public void adauga_profesor(Profesor p)
+        {
+
+        }
+        public void adauga_student(Student s)
+        {
+
+        }
+
+       public  void medie_student_disciplina(Disciplina d,Student s)
+        {
+
+        }
+
+       public void raport_disciplina(Disciplina d)
+        {
+
+        }
+
+       public void raport_pe_an(int an)
+        {
+
+        }
+
+       public void raport_promovabilitate(int an)
+        {
+
+        }
+      
+
+    }
+}
---
+++ Bogdana/PSSC/Models/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class Student
+    {
+        string nume;
+        string telefon;
+        string email;
+        int an_studiu;
+        List<Disciplina> listaDiscipline = new List<Disciplina>();
+        List<Nota> listaNote = new List<Nota>();
+
+        public Student(string nume, string telefon, string email,int an_studiu)
+        {
+            this.nume = nume;
+            this.telefon = telefon;
+            this.email = email;
+            this.an_studiu = an_studiu;
+        }
+        public string Nume
+        {
+            get { return nume; }
+            set { nume = value; }
+        }
+
+        public string Telefon
+        {
+            get { return telefon; }
+            set { telefon = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public int An_studiu
+        {
+            get { return an_studiu; }
+            set { an_studiu = value; }
+        }
+    }
+}
